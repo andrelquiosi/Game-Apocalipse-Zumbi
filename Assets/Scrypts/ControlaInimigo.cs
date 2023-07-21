96b970d5bdd3fa368114fc5a32af2e83111f1c72
andrelquiosi <andre.quiosi@outlook.com>
2023-07-21 13:13:47 -0300

    Adicionando movimentação aos Inimigos. Colocando os inimigos para seguirem o jogador.

---
+++ Assets/Scrypts/ControlaInimigo.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaInimigo : MonoBehaviour
+{
+    public GameObject jogador;
+    public float velicidade = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+
+        Vector3 direcao = jogador.transform.position - transform.position;
+
+        GetComponent<Rigidbody>()
+        .MovePosition(GetComponent<Rigidbody>()
+        .position + direcao.normalized * velicidade * Time.deltaTime);
+    }
+}
