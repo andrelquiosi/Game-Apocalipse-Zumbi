ed1856e86ffdd87fc513d75a3e380bd52859a01c
andrelquiosi <andre.quiosi@outlook.com>
2023-07-22 12:03:36 -0300

    -Fazendo o Inimigo sempre olhar para o jogador. -Usando a medição de distância para o inimigo não empurrar o jogador.

--- Assets/Scrypts/ControlaInimigo.cs
+++ Assets/Scrypts/ControlaInimigo.cs
@@ -21,10 +21,20 @@ public class ControlaInimigo : MonoBehaviour
     void FixedUpdate()
     {
 
-        Vector3 direcao = jogador.transform.position - transform.position;
 
-        GetComponent<Rigidbody>()
-        .MovePosition(GetComponent<Rigidbody>()
-        .position + direcao.normalized * velicidade * Time.deltaTime);
+        float distancia = Vector3.Distance(transform.position, jogador.transform.position);
+
+        if (distancia > 2.5)
+        {
+
+            Vector3 direcao = jogador.transform.position - transform.position;
+
+            GetComponent<Rigidbody>()
+            .MovePosition(GetComponent<Rigidbody>()
+            .position + direcao.normalized * velicidade * Time.deltaTime);
+        
+        Quaternion novaRotacao = Quaternion.LookRotation(direcao);
+            GetComponent<Rigidbody>().MoveRotation(novaRotacao);
+        }
     }
 }
