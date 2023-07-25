00e7da59179378000e953a99989b73fb33744289
andrelquiosi <andre.quiosi@outlook.com>
2023-07-25 20:14:29 -0300

    Colocando a animação de ataque nos inimigos. Fazendo atacar o Personagem.

--- Assets/Scrypts/ControlaInimigo.cs
+++ Assets/Scrypts/ControlaInimigo.cs
@@ -23,18 +23,22 @@ public class ControlaInimigo : MonoBehaviour
 
 
         float distancia = Vector3.Distance(transform.position, jogador.transform.position);
+        Vector3 direcao = jogador.transform.position - transform.position;
+
+        Quaternion novaRotacao = Quaternion.LookRotation(direcao);
+        GetComponent<Rigidbody>().MoveRotation(novaRotacao);
 
         if (distancia > 2.5)
         {
 
-            Vector3 direcao = jogador.transform.position - transform.position;
-
             GetComponent<Rigidbody>()
             .MovePosition(GetComponent<Rigidbody>()
             .position + direcao.normalized * velicidade * Time.deltaTime);
-        
-        Quaternion novaRotacao = Quaternion.LookRotation(direcao);
-            GetComponent<Rigidbody>().MoveRotation(novaRotacao);
+            GetComponent<Animator>().SetBool("Atacando", false);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("Atacando", true);
         }
     }
 }
